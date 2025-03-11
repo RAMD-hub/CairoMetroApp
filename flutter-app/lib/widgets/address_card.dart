a0d820e84bf3ab6e03ad4f7ebea56650ecfc8388
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-03-11 17:06:46 +0200

    Added AddressCard for better user input regarding locations.

---
+++ flutter-app/lib/widgets/address_card.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'custom_button.dart';
+import 'custom_text.dart';
+import 'custom_text_field.dart';
+
+class AddressCard extends StatelessWidget {
+  const AddressCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      elevation: 5,
+      shadowColor: Colors.grey,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          spacing: 16,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CustomText(
+              text: 'Please enter your destination (optional)',
+              txtColor: Colors.black87,
+            ),
+            const CustomTextField(
+              hint: 'Address',
+              suffixIcon: Icons.location_city_outlined,
+            ),
+            CustomButton(onPressed: () {}, btnName: 'Search')
+          ],
+        ),
+      ),
+    );
+  }
+}
