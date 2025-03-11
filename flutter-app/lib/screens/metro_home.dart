24a09842f02a041c25d6f389796a164fd91360d9
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-03-11 17:01:58 +0200

    Refactored MetroHome Screen & Added Multiple Widgets:     - Replaced SingleChildScrollView with CustomScrollView for better scrolling. - Implemented SliverAppBar to keep the app bar visible while scrolling.

--- flutter-app/lib/screens/metro_home.dart
+++ flutter-app/lib/screens/metro_home.dart
@@ -1,29 +1,64 @@
+import 'package:cairo_metro_flutter/widgets/custom_button.dart';
+import 'package:cairo_metro_flutter/widgets/custom_text_field.dart';
+import 'package:cairo_metro_flutter/widgets/dialog_card.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../widgets/address_card.dart';
+import '../widgets/custom_icon.dart';
+import '../widgets/custom_text.dart';
+import '../widgets/stations_card.dart';
 
 class MetroHome extends StatelessWidget {
-  const MetroHome({super.key});
+  MetroHome({super.key});
+  final selectedTransfers = 'Less Stations'.obs;
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Icon(
-                Icons.location_on_outlined,
-                color: Color(0xFFFEA613),
-                size: screenWidth * 0.07,
-              ),
-              Icon(
-                Icons.language,
-                color: Color(0xFFFEA613),
-                size: screenWidth * 0.07,
+      backgroundColor: const Color(0xFFF7F6F2),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            pinned: true,
+            floating: false,
+            leading: const Padding(
+              padding: EdgeInsets.only(left: 16.0),
+              child: CustomIcon(icon: Icons.location_on_outlined),
+            ),
+            actions: const [
+              Padding(
+                padding: EdgeInsets.only(right: 16.0),
+                child: CustomIcon(icon: Icons.language, color: Colors.black),
               ),
             ],
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: const CustomText(
+                        text: 'Welcome', txtColor: Color(0xFFFEA613)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: const CustomText(text: 'Where are you going Today?'),
+                  ),
+                  const DialogCard(
+                    previousStation: 'New ElMarg',
+                    currentStation: 'El Marg',
+                    nextStation: 'Sakanat El-Maadi',
+                  ),
+                  StationsCard(selectedTransfers: selectedTransfers),
+                  AddressCard(),
+                ],
+              ),
+            ),
           ),
         ],
       ),
