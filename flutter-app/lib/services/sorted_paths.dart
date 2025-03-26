4c0697d14d1d963eb14319ac6f7a26d9828eac95
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-03-26 17:42:55 +0200

    Refactor sorting function using dartx for cleaner and more readable code
    Description:
    
    Replaced manual sorting with sortedBy and thenBy from dartx.

--- flutter-app/lib/services/sorted_paths.dart
+++ flutter-app/lib/services/sorted_paths.dart
@@ -1,28 +1,23 @@
+import 'package:dartx/dartx.dart';
+
 import '../models/metro_path.dart';
 
 class SortedPaths {
   List<List<String>> sortMetroPathsByLengthOfStations(
       List<MetroPath> metroPaths) {
-    metroPaths.sort((a, b) {
-      if (a.path.length != b.path.length) {
-        return a.path.length.compareTo(b.path.length);
-      }
-      return a.exchangedStationList.length
-          .compareTo(b.exchangedStationList.length);
-    });
-    return metroPaths.map((e) => e.path).toList();
+    return metroPaths
+        .sortedBy((e) => e.path.length)
+        .thenBy((e) => e.exchangedStationList.length)
+        .map((e) => e.path)
+        .toList();
   }
 
   List<List<String>> sortMetroPathsByExchangeStations(
       List<MetroPath> metroPaths) {
-    metroPaths.sort((a, b) {
-      if (a.exchangedStationList.length != b.exchangedStationList.length) {
-        return a.exchangedStationList.length
-            .compareTo(b.exchangedStationList.length);
-      }
-      return a.path.length.compareTo(b.path.length);
-    });
-
-    return metroPaths.map((e) => e.path).toList();
+    return metroPaths
+        .sortedBy((e) => e.exchangedStationList.length)
+        .thenBy((e) => e.path.length)
+        .map((e) => e.path)
+        .toList();
   }
 }
